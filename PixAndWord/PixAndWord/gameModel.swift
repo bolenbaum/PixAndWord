43228d7e67f20685002f21531681fa2914796e1f
matthewbolenbaugh <bolenbaum@sou.edu>
2018-11-08 11:33:37 -0800

    created gameModel.swift file

---
+++ PixAndWord/PixAndWord/gameModel.swift
@@ -0,0 +1,16 @@
+//
+//  gameModel.swift
+//  PixAndWord
+//
+//  Created by Matthew Bolenbaugh on 11/8/18.
+//  Copyright © 2018 SOU. All rights reserved.
+//
+
+import Foundation
+
+class gameModel {
+    static let sharedInstance = gameModel()
+    
+    
+}
+
