5bdad1495849abf9f914e8d58f5681dbb2c65882
Nick Morales <nico@nicks-macbook-air.local>
2018-11-11 22:39:17 -0800

    Added counter

--- PixAndWord/PixAndWord/GameScene.swift
+++ PixAndWord/PixAndWord/GameScene.swift
@@ -25,18 +25,26 @@ class GameScene: SKScene{
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+    private var counter: SKLabelNode?
+    var counterCount: Int = 4000{
+        didSet{
+        counter?.text = "\(counterCount)"
+        }
+    }
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
-        
+        counter?.fontColor = SKColor.white
         // Get label node from scene and store it for use later
+        self.counter = self.childNode(withName: "counter") as? SKLabelNode
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
-        
+        if let counter = self.counter{
+            counter.text = ""
+        }
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
