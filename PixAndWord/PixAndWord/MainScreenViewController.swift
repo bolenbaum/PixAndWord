cd084d5438c42f004e24846e95e72a011996fb5e
matthewbolenbaugh <bolenbaum@sou.edu>
2018-11-12 23:58:48 -0800

    Created viewControllers for main menu, hint, and give up screens. Also, changed initial screen to main menu. Added a give up button. Need to implement collection views for the letter tiles and for the four pics on the game screen.

---
+++ PixAndWord/PixAndWord/MainScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainScreenViewController.swift
+//  PixAndWord
+//
+//  Created by Matthew Bolenbaugh on 11/12/18.
+//  Copyright © 2018 SOU. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    
+    @IBAction func newGame(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
