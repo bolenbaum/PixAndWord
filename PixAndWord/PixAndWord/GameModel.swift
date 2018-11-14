ea80b9999befc46caea9dd13cfd087b6810a000d
matthewbolenbaugh <bolenbaum@sou.edu>
2018-11-14 10:30:08 -0800

    worked on initializer for GameModel and created two functions, one to turn a string into chars and a second to randomly select chars from alphabet

--- PixAndWord/PixAndWord/GameModel.swift
+++ PixAndWord/PixAndWord/GameModel.swift
@@ -9,9 +9,31 @@
 import Foundation
 
 class GameModel {
+    
+    
     static let sharedInstance = GameModel()
+    var letters: []
+    
     let levels = [Level(levelNum: 1, wordForLevel: "apple", filePaths: [""]), Level(levelNum: 2, wordForLevel: "baseball", filePaths: [""]), Level(levelNum: 3, wordForLevel: "pokemon", filePaths: [""]), Level(levelNum: 4, wordForLevel: "turquoise", filePaths: [""])]
     
+    init(letters: [], level: [Level]) {
+        self.levels = level
+        self.letters = letters
+    }
+    
+    func wordToLetters() -> Array<Character> {
+        let chars = Array(GameModel.sharedInstance.levels.wordForLevel)
+        letters = chars
+        
+        return letters
+    }
+    
+    func pickRandLetter() -> Array<Character> {
+        var n: Int = GameModel.sharedInstance.letters.count
+        
+        var size: Int = 2*n+1
+        
+    }
     
 }
 
