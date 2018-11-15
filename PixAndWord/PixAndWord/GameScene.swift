231b5dd89ce273899bf4359e8d2765da677a7cdc
Nick Morales <nico@nicks-macbook-air.local>
2018-11-15 11:52:15 -0800

    added physics bodies to game

--- PixAndWord/PixAndWord/GameScene.swift
+++ PixAndWord/PixAndWord/GameScene.swift
@@ -11,13 +11,14 @@ import GameplayKit
 
 class GameScene: SKScene{
     override func didMove(to view: SKView) {
-        
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
     }
-    struct  Physics{
+    struct  PhysicsCategory{
         static let none      : UInt32 = 0
         static let all       : UInt32 = UInt32.max
-        static let holder   : UInt32 = 0b1       // 1
-        static let tiles: UInt32 = 0b10
+        static let tile   : UInt32 = 0b1       // 1
+        static let letter: UInt32 = 0b10
     }
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
@@ -55,10 +56,8 @@ class GameScene: SKScene{
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
         if let spinnyNode = self.spinnyNode {
             spinnyNode.lineWidth = 2.5
-            
             spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
@@ -66,8 +65,6 @@ class GameScene: SKScene{
         }
         addTiles()
     }
-    
-    
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -75,7 +72,6 @@ class GameScene: SKScene{
             self.addChild(n)
         }
     }
-    
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -83,7 +79,6 @@ class GameScene: SKScene{
             self.addChild(n)
         }
     }
-    
     func touchUp(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
@@ -91,28 +86,21 @@ class GameScene: SKScene{
             self.addChild(n)
         }
     }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-    
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
@@ -120,20 +108,22 @@ class GameScene: SKScene{
         if (self.lastUpdateTime == 0) {
             self.lastUpdateTime = currentTime
         }
-        
         // Calculate time since last update
         let dt = currentTime - self.lastUpdateTime
-        
         // Update entities
         for entity in self.entities {
             entity.update(deltaTime: dt)
         }
-        
         self.lastUpdateTime = currentTime
     }
     func addTiles(){
         let tile = SKSpriteNode(imageNamed: "Assest.xcassets/Images/GameObjects/Tile.png")
         addChild(tile)
+        tile.physicsBody = SKPhysicsBody(rectangleOf: tile.size)
+        tile.physicsBody?.isDynamic = true;
+        tile.physicsBody?.categoryBitMask = PhysicsCategory.tile
+        tile.physicsBody?.contactTestBitMask = PhysicsCategory.letter
+        tile.physicsBody?.collisionBitMask = PhysicsCategory.none
     }
     func addLetters(){
         var tex = GameModel.sharedInstance.wordToLetters(word: GameModel.sharedInstance.levels[GameModel.sharedInstance.levNum].word)
@@ -141,9 +131,9 @@ class GameScene: SKScene{
             let letter = SKSpriteNode(imageNamed: GameModel.sharedInstance.sendFilePath(fileName: tex[i]))
             GameModel.sharedInstance.addRealLet()
             addChild(letter)
+            letter.physicsBody = SKPhysicsBody(rectangleOf: letter.size)
         }
     }
-    
 }
 extension GameScene: SKPhysicsContactDelegate{
     
