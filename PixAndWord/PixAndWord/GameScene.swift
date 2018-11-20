3fb9628c165b5f1ee211d0f340d33953a4e564f6
Nick Morales <nico@nicks-macbook-air.local>
2018-11-19 20:52:11 -0800

    Improved image overlay Also there are more tiles

--- PixAndWord/PixAndWord/GameScene.swift
+++ PixAndWord/PixAndWord/GameScene.swift
@@ -48,59 +48,30 @@ class GameScene: SKScene{
         //addChild(counter!)
         //runTimer()
         //updateTimer()
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
         addTiles()
         addLetters()
     }
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+       
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -118,18 +89,26 @@ class GameScene: SKScene{
         self.lastUpdateTime = currentTime
     }
     func addTiles(){
-        let tile = SKSpriteNode(imageNamed: "Images/GameObjects/Tile.png")
-        tile.setScale(0.07)
-        let yPos: CGFloat = 220
-        tile.position = CGPoint(x: 1.1, y: yPos)
-        
-       // tile.position =
-        addChild(tile)
-        tile.physicsBody = SKPhysicsBody(rectangleOf: tile.size)
-        tile.physicsBody?.isDynamic = true;
-        tile.physicsBody?.categoryBitMask = PhysicsCategory.tile
-        tile.physicsBody?.contactTestBitMask = PhysicsCategory.letter
-        tile.physicsBody?.collisionBitMask = PhysicsCategory.none
+        for i in stride(from: 0, to: GameModel.sharedInstance.levels[GameModel.sharedInstance.levNum].word.count, by: 1){
+            let ini = CGFloat(-141.1)
+            let tile = SKSpriteNode(imageNamed: "Images/GameObjects/Tile.png")
+            tile.setScale(0.07)
+            let yPos: CGFloat = 220
+            if i > 0{
+                tile.position = CGPoint(x: ini + CGFloat(Double(tile.size.width) * Double(i)), y: yPos)
+                
+            }
+            else{
+                tile.position = CGPoint(x: ini, y: yPos)
+            }
+           // tile.position =
+            addChild(tile)
+            tile.physicsBody = SKPhysicsBody(rectangleOf: tile.size)
+            tile.physicsBody?.isDynamic = true;
+            tile.physicsBody?.categoryBitMask = PhysicsCategory.tile
+            tile.physicsBody?.contactTestBitMask = PhysicsCategory.letter
+            tile.physicsBody?.collisionBitMask = PhysicsCategory.none
+        }
     }
     func random() -> CGFloat {
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
